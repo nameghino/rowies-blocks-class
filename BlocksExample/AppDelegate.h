be673e968c8125c78af8c2b6b378bfab317ba299
Nico Ameghino <nicolas.ameghino@gmail.com>
2015-05-20 23:30:31 -0300

    Initial Commit

---
+++ BlocksExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocksExample
+//
+//  Created by Nicolas Ameghino on 5/20/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocksExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocksExample
+//
+//  Created by Nicolas Ameghino on 5/20/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
